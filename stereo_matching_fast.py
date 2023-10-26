3651703c37a2ca15160d955404666f6bac5f74be
Simon Haaning <73152757+SimonHaaningAAU@users.noreply.github.com>
2023-10-26 10:31:44 +0200

    Added depth computation and plots

--- stereo_matching_fast.py
+++ stereo_matching_fast.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
-import sys
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 
 def preprocess_frame(frame):
+    # Equalize histogram to improve contrast
     frame = cv2.equalizeHist(frame)
+
+    # Apply a Gaussian blur to reduce noise while preserving edges
     frame = cv2.GaussianBlur(frame, (5, 5), 0)
     return frame
 
 
 def shift_image(image, shift):
-    # shift image
+    # Shift image a certain number of pixels
     translation_matrix = np.float32([[1, 0, shift], [0, 1, 0]])
     shifted_image = cv2.warpAffine(
         image,
@@ -22,11 +24,10 @@ def shift_image(image, shift):
     return shifted_image
 
 
-def calculate_disparity_gradients(
-    left_image, right_image, num_disparities=64, window_size=15
-):
-    window = np.ones([window_size, window_size]) / window_size
+def calc_disparity_grad(left_image, right_image, num_disparities=64, window_size=15):
+    window = np.ones([window_size, window_size]) / (window_size**2)
 
+    # Initialize disparity maps
     disparity_maps = np.zeros(
         [left_image.shape[0], left_image.shape[1], num_disparities]
     )
@@ -37,57 +38,70 @@ def calculate_disparity_gradients(
     grad_x_right = np.gradient(right_image, axis=1)
     grad_y_right = np.gradient(right_image, axis=0)
 
-    for d in tqdm(range(0, num_disparities)):
-        # shift right image
-        grad_x_right_shifted = shift_image(grad_x_right, d)
-        grad_y_right_shifted = shift_image(grad_y_right, d)
+    for disparity in tqdm(
+        range(num_disparities), desc="Computing disparity (gradients)"
+    ):
+        # Shift right image
+        grad_x_right_shifted = shift_image(grad_x_right, disparity)
+        grad_y_right_shifted = shift_image(grad_y_right, disparity)
 
-        # calculate squared differences
+        # Calculate Sum of Absolute Differences (SAD)
         SAD_x = abs(np.float32(grad_x_left) - np.float32(grad_x_right_shifted))
         SAD_y = abs(np.float32(grad_y_left) - np.float32(grad_y_right_shifted))
         SAD = SAD_x + SAD_y
 
-        # convolve with kernel and find SAD at each point
+        # Convolve with kernel and find SAD at each region
         filtered_image = cv2.filter2D(SAD, -1, window)
+        disparity_maps[:, :, disparity] = filtered_image
 
-        disparity_maps[:, :, d] = filtered_image
-
+    # Select disparity with minimum SAD
     disparity = np.argmin(disparity_maps, axis=2)
-    disparity = np.uint8(disparity * 255 / num_disparities)
-    disparity = cv2.equalizeHist(disparity)
+    disparity = np.uint16(disparity * 1023 / num_disparities)
 
     return disparity
 
 
-def calculate_disparity(left_image, right_image, num_disparities=64, window_size=15):
-    window = np.ones([window_size, window_size]) / window_size
+def calc_disparity(left_image, right_image, num_disparities=64, window_size=15):
+    window = np.ones([window_size, window_size]) / (window_size**2)
 
+    # Initialize disparity maps
     disparity_maps = np.zeros(
         [left_image.shape[0], left_image.shape[1], num_disparities]
     )
 
-    for d in tqdm(range(0, num_disparities)):
-        # shift right image
-        right_image_shifted = shift_image(right_image, d)
+    for disparity in tqdm(
+        range(num_disparities), desc="Computing disparity            "
+    ):
+        # Shift right image
+        right_image_shifted = shift_image(right_image, disparity)
 
-        # calculate squared differences
+        # Calculate Sum of Absolute Differences (SAD)
         SAD = abs(np.float32(left_image) - np.float32(right_image_shifted))
 
-        # convolve with kernel and find SAD at each point
+        # Convolve with kernel and find SAD at each region
         filtered_image = cv2.filter2D(SAD, -1, window)
+        disparity_maps[:, :, disparity] = filtered_image
 
-        disparity_maps[:, :, d] = filtered_image
-
+    # Select disparity with minimum SAD
     disparity = np.argmin(disparity_maps, axis=2)
-    disparity = np.uint8(disparity * 255 / num_disparities)
-    disparity = cv2.equalizeHist(disparity)
+    disparity = np.uint16(disparity * 1023 / num_disparities)
 
     return disparity
+
+
+def disparity_to_depth(disparity, baseline, focal_length_pixels):
+    depth = np.zeros_like(disparity, dtype=np.float32)
+    depth[disparity > 64] = (baseline * focal_length_pixels) / disparity[disparity > 64]
+    return depth
 
 
 # Define stereo parameters
 num_disparities = 64  # number of disparities to check
 window_size = 15  # block size to match
+baseline = 0.42  # baseline between the two cameras in m
+focal_length = 14.67e-3  # focal length of camera in m
+pixel_size = 12e-6  # pixel size of camera in m
+focal_length_pixels = focal_length / pixel_size  # focal length in pixels
 
 # left_image_path = "Images/LeftNavCam.jpg"
 # right_image_path = "Images/RightNavCam.jpg"
@@ -98,23 +112,55 @@ right_image = cv2.imread(right_image_path, cv2.IMREAD_GRAYSCALE)
 left_image = preprocess_frame(left_image)
 right_image = preprocess_frame(right_image)
 
-# Plot for comparison
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-
 # Disparity without using gradients
-disparity = calculate_disparity(left_image, right_image, num_disparities, window_size)
-ax[0].imshow(disparity, cmap="gray")
+disp = calc_disparity(left_image, right_image, num_disparities, window_size)
+depth = disparity_to_depth(disp, baseline, focal_length_pixels)
 
 # Disparity using gradients
-disparity = calculate_disparity_gradients(
-    left_image, right_image, num_disparities, window_size
-)
-ax[1].imshow(disparity, cmap="gray")
+disp_gd = calc_disparity_grad(left_image, right_image, num_disparities, window_size)
+depth_gd = disparity_to_depth(disp_gd, baseline, focal_length_pixels)
 
 # Disparity using OpenCV
 stereo = cv2.StereoBM_create(num_disparities, window_size)
-disparity = stereo.compute(left_image, right_image)
-ax[2].imshow(disparity, cmap="gray")
+disp_cv = stereo.compute(left_image, right_image)
+depth_cv = disparity_to_depth(disp_cv, baseline, focal_length_pixels)
+
+# Plot the results for comparison
+fig, ax = plt.subplots(2, 3, figsize=(15, 8))
+fig.tight_layout()
+ax[0, 0].set_title("Disparity without gradients")
+ax[0, 1].set_title("Disparity with gradients")
+ax[0, 2].set_title("Disparity using OpenCV")
+
+# Plot limits
+vmin_disp = 0
+vmax_disp = np.max([np.max(disp), np.max(disp_gd), np.max(disp_cv)])
+vmin_dist = 0
+vmax_dist = np.max([np.max(depth), np.max(depth_gd), np.max(depth_cv)])
+
+# Create plots
+ax[0, 0].imshow(disp, cmap="gray", vmin=vmin_disp, vmax=vmax_disp)
+ax[1, 0].imshow(depth, cmap="viridis", vmin=vmin_dist, vmax=vmax_dist)
+ax[0, 1].imshow(disp_gd, cmap="gray", vmin=vmin_disp, vmax=vmax_disp)
+ax[1, 1].imshow(depth_gd, cmap="viridis", vmin=vmin_dist, vmax=vmax_dist)
+ax[0, 2].imshow(disp_cv, cmap="gray", vmin=vmin_disp, vmax=vmax_disp)
+ax[1, 2].imshow(depth_cv, cmap="viridis", vmin=vmin_dist, vmax=vmax_dist)
+
+# Create colorbars for each subplot
+cbar_disp = fig.colorbar(ax[0, 0].images[0], ax=ax[0, 0], shrink=0.8)
+cbar_dist = fig.colorbar(ax[1, 0].images[0], ax=ax[1, 0], shrink=0.8)
+cbar_disp_gd = fig.colorbar(ax[0, 1].images[0], ax=ax[0, 1], shrink=0.8)
+cbar_dist_gd = fig.colorbar(ax[1, 1].images[0], ax=ax[1, 1], shrink=0.8)
+cbar_disp_cv = fig.colorbar(ax[0, 2].images[0], ax=ax[0, 2], shrink=0.8)
+cbar_dist_cv = fig.colorbar(ax[1, 2].images[0], ax=ax[1, 2], shrink=0.8)
+
+# Set colorbar labels
+cbar_disp.set_label("Disparity [px]")
+cbar_dist.set_label("Depth [m]")
+cbar_disp_gd.set_label("Disparity [px]")
+cbar_dist_gd.set_label("Depth [m]")
+cbar_disp_cv.set_label("Disparity [px]")
+cbar_dist_cv.set_label("Depth [m]")
 
 # Show the plot
 plt.waitforbuttonpress()
